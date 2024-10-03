fe9aac36c803a2d79ff15acb754095be6307c205
Pedro Oliveira <pedro.o.santos7@aluno.senai.br>
2024-10-03 09:02:04 -0300

    refactor: alter server to use routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -10,11 +10,4 @@ routes.get("/", (req, res) => {
     return res.status(200).send({ message: "Hello, World!" })
 });
 
-routes.get("/2tds2", (req, res) => {
-    return res.status(200).send({ message: "Hello, World!" })
-});
-
-routes.use("/emocoes", emocoesRoutes);
-routes.use("/personagens", personagensRoutes);
-
 export default routes;
--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
-import express from "express";
-import { config } from "dotenv";
+import express from "express"
+import {config} from "dotenv"
+
+import routes from "./routes/index.routes.js";
 
 config();
 
-const serverPort = process.env.PORT || 3000;
+const serverPort = process.env.PORT || 3000
 
 const app = express();
 app.use(express.json());
+app.use(routes);
 
-app.get("/", (req, res) => {
-  return res.status(200).json({ message: "Hello, World!" });
-});
 
 app.listen(serverPort, () => {
-  console.log(`🐧 Server started on http://localhost:${serverPort}`);
-});
+    console.log(`🐧 Server started on http://localhost:${serverPort}`)
+})
